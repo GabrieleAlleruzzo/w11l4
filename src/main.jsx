15034478bef57796d0db974fd02732240460f3a3
Gabriele Alleruzzo <alleruzzo.gabriele.96@gmail.com>
2025-04-17 16:32:23 +0200

    test error

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store, persistedStore } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    import {(store, persistedStore)} from "./redux/store";
+    <PersistGate persistor="persist">
+      <App />
+    </PersistGate>
   </Provider>
-)
+);
